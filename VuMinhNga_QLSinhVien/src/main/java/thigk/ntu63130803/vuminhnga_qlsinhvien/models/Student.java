b050f7c5c2cda1b04dcb2d6106613f25f299f15a
chimcuccu258 <vuminhnga25@gmail.com>
2024-03-26 15:25:46 +0700

    update GK

---
+++ VuMinhNga_QLSinhVien/src/main/java/thigk/ntu63130803/vuminhnga_qlsinhvien/models/Student.java
@@ -0,0 +1,12 @@
+package thigk.ntu63130803.vuminhnga_qlsinhvien.models;
+/*
+ * @created 26/03/2024 - 15:24
+ * @project VuMinhNga_QLSinhVien
+ * @author TaosDev
+ */
+
+public class Student {
+    private String studentId;
+    private String name;
+    private String gpa;
+}
